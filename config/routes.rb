1703b143439acf8ff0427bda7fe578243653f485
Xiaopow <xiaoyang88@gmail.com>
2018-11-26 18:34:03 +0800

    basic front-end app complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
 
-  # Add routes below this line
-  resources :users, only: [:create]
+  namespace :api do
+    # Add routes below this line
+    resources :users, only: [:create]
 
-  get    'tasks/:id'                => 'tasks#show'
-  get    'tasks'                    => 'tasks#index'
-  post   'tasks'                    => 'tasks#create'
-  put    'tasks/:id'                => 'tasks#update'
-  put    'tasks/:id/mark_complete'  => 'tasks#mark_complete'
-  put    'tasks/:id/mark_active'    => 'tasks#mark_active'
-  delete 'tasks/:id'                => 'tasks#destroy'
+    get    'tasks/:id'                => 'tasks#show'
+    get    'tasks'                    => 'tasks#index'
+    post   'tasks'                    => 'tasks#create'
+    put    'tasks/:id'                => 'tasks#update'
+    put    'tasks/:id/mark_complete'  => 'tasks#mark_complete'
+    put    'tasks/:id/mark_active'    => 'tasks#mark_active'
+    delete 'tasks/:id'                => 'tasks#destroy'
+  end
+
+  root 'static_pages#index'
+
+  # Redirect all other paths to index page, which will be taken over by AngularJS
+  get '*path'    => 'static_pages#index'
 end
