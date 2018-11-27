9e201e238c3f73b01f417089dd224920752cbc12
williamwlchan <william.wlchan@outlook.com>
2018-11-27 18:55:51 +0800

    Add all buttons

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,10 +2,11 @@ $(document).on("turbolinks:load", function () {
   if ($('.static_pages.index').length > 0) {
     indexTasks(function (response) {
       var htmlString = response.tasks.map(function(task) {
-        return "<div class='col-12 col-sm-6 mb-3 p-2 task' data-id='" + task.id + "'>" + task.content + "</div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-danger'>Remove</button></div>";
+        return "<div class='col-12 col-sm-3 mb-3 p-2 task' data-id='" + task.id + "'>" + task.content + "</div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-success'>Mark Complete</button></div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-primary'>Mark Active</button></div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-danger'>Remove</button></div>";
       });
 
       $("#tasks").html(htmlString);
     });
   }
+
 });
