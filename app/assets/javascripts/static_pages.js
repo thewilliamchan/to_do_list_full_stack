245ce098c48374506a3c8693eafd5138c4786bed
williamwlchan <william.wlchan@outlook.com>
2018-11-27 21:11:11 +0800

    Add nav link

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,8 +1,8 @@
 $(document).on("turbolinks:load", function () {
   if ($(".static_pages.index").length > 0 && $(".allTasks").hasClass("active")) {
     indexTasks(function (response) {
+      var htmlString = "";
       response.tasks.forEach(function (task) {
-        var htmlString = "";
         if (!task.completed) {
           htmlString += "<div class='col-12 col-sm-6 mb-3 p-2 task' data-id='" + task.id + "'>" + task.content + "</div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-success'>Mark Complete</button></div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-danger'>Remove</button></div>";
         } else if (task.completed) {
@@ -16,8 +16,8 @@ $(document).on("turbolinks:load", function () {
 
   if ($(".static_pages.index").length > 0 && $(".activeTasks").hasClass("active")) {
     indexTasks(function (response) {
+      var htmlString = "";
       response.tasks.forEach(function (task) {
-        var htmlString = "";
         if (!task.completed) {
           htmlString += "<div class='col-12 col-sm-6 mb-3 p-2 task' data-id='" + task.id + "'>" + task.content + "</div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-success'>Mark Complete</button></div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-danger'>Remove</button></div>";
         }
@@ -29,8 +29,8 @@ $(document).on("turbolinks:load", function () {
 
   if ($(".static_pages.index").length > 0 && $(".completedTasks").hasClass("active")) {
     indexTasks(function (response) {
+      var htmlString = "";
       response.tasks.forEach(function (task) {
-        var htmlString = "";
         if (task.completed) {
           htmlString += "<div class='col-12 col-sm-6 mb-3 p-2 text-success task' data-id='" + task.id + "'>" + task.content + "</div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-success'>Mark Complete</button></div><div class='col-12 col-sm-3 mb-3 p-2'><button type='submit' class='btn btn-danger'>Remove</button></div>";
         }
