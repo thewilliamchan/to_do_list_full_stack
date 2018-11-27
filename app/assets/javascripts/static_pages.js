e41d95ae351c58bada31fb0acae341c1067ac0cd
williamwlchan <william.wlchan@outlook.com>
2018-11-27 18:32:10 +0800

    Add remove button

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,7 +2,7 @@ $(document).on("turbolinks:load", function () {
   if ($('.static_pages.index').length > 0) {
     indexTasks(function (response) {
       var htmlString = response.tasks.map(function(task) {
-        return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'>" + task.content + "</div><span class='input-group-btn'><button type='submit' class='btn'>Remove</button></span>";
+        return "<div class='col-12 mb-3 p-2'><div class='border rounded task' data-id='" + task.id + "'>" + task.content + "</div><span class='input-group-btn'><button type='submit' class='btn btn-danger'>Remove</button></span></div>";
       });
 
       $("#tasks").html(htmlString);
